85048bbb2b0385590bd2d7b9ce031facd14093f2
Austin West <korruptedanomaly@gmail.com>
2019-06-17 15:26:10 -0600

    Alarm.swift model set up

---
+++ AlarmiOS27/AlarmiOS27/MODEL/Alarm.swift
@@ -0,0 +1,31 @@
+//
+//  Alarm.swift
+//  AlarmiOS27
+//
+//  Created by Austin West on 6/17/19.
+//  Copyright © 2019 Austin West. All rights reserved.
+//
+
+import Foundation
+
+class Alarm {
+    
+    var fireDate: Date
+    var name: String
+    var enabled: Bool
+    var uuid: String
+    
+    init(fireDate: Date, name: String, enabled: Bool, uuid: String = UUID().uuidString) {
+        self.fireDate = fireDate
+        self.name = name
+        self.enabled = enabled
+        self.uuid = uuid
+    }
+    
+    var fireTimeAsString: String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .none
+        formatter.timeStyle = .short
+        return formatter.string(from: fireDate)
+    }
+}
